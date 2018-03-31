0fd5f4f7d5c1cd619d1e1fd60af29d570ccd8cc7
agpiermarini <piermarini.andrew@gmail.com>
2018-03-30 23:20:52 -0600

    Add highest rated review information to show page

--- spec/features/books/user_can_see_a_book_page_spec.rb
+++ spec/features/books/user_can_see_a_book_page_spec.rb
@@ -68,9 +68,31 @@ describe 'User' do
 
       expect(page).to have_content("Average Rating: #{book.average_rating}")
     end
+
+    it 'sees rating, body and reviewer name for the review with the highest rating' do
+      book = Book.create!(title: "Book Title")
+      user_1 = User.create!(name: "User_1")
+      user_2 = User.create!(name: "User_2")
+      user_3 = User.create!(name: "User_3")
+      user_4 = User.create!(name: "User_4")
+      user_5 = User.create!(name: "User_5")
+      user_6 = User.create!(name: "User_6")
+      review_1 = user_1.reviews.create!(body: "Really great book", rating: 5, book_id: book.id)
+      review_2 = user_2.reviews.create!(body: "Really bad book", rating: 1, book_id: book.id)
+      review_3 = user_3.reviews.create!(body: "Really wonderful book", rating: 4, book_id: book.id)
+      review_4 = user_4.reviews.create!(body: "Really long book", rating: 3, book_id: book.id)
+      review_5 = user_5.reviews.create!(body: "Really boring book", rating: 2, book_id: book.id)
+      review_6 = user_6.reviews.create!(body: "Really fantastic book", rating: 5, book_id: book.id)
+
+      visit book_path(book)
+
+      within(".highest_rating") do
+        expect(page).to have_content("Highest Rating: #{book.highest_rating.rating}")
+        expect(page).to have_content("Reviewer: #{book.highest_rating.user.name}")
+        expect(page).to have_content("Review: #{book.highest_rating.body}")
+      end
+    end
   end
-
-
 end
 
 
